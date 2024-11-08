7a48ae0a93af54ea8c5cdba72005b538c1646eed
AmrahovaDilber <amrahovad911@gmail.com>
2024-11-08 11:43:20 +0400

    Created Footer Component

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
@@ -8,6 +9,9 @@ const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         <Header />
       </header>
       <main className="flex-1 bg-[#0b0b0b]  flex-col ">{children}</main>
+      <footer>
+        <Footer></Footer>
+    </footer>
     </div>
   );
 };
